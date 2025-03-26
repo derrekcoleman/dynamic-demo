9b5414588f0e4e1f507ebe7ef05eeeef9688b626
Derrek <80121818+derrekcoleman@users.noreply.github.com>
2025-03-26 17:26:43 -0600

    Configure "embedded wallets" and "account abstraction"
    Updates the Dynamic SDK configuration at the same time as some dashboard
    updates.
    
    1. Enables "embedded wallets", which automatically creates a blockchain
    wallet for the user during a familiar email sign-in process.
    https://docs.dynamic.xyz/wallets/embedded-wallets/overview-tee-wallets
    
    2. Also enables "account abstraction", which makes it free for users to
    mint NFTs, send funds to other people, and other possible transactions.
    https://docs.dynamic.xyz/smart-wallets/smart-wallet-providers/zerodev

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import { ZeroDevSmartWalletConnectorsWithConfig } from "@dynamic-labs/ethereum-aa";
 import Main from "./Main";
 
 const App = () => (
   <DynamicContextProvider
     theme="auto"
     settings={{
-      environmentId: "2762a57b-faa4-41ce-9f16-abff9300e2c9",
-      walletConnectors: [EthereumWalletConnectors],
+      environmentId: "8005230e-7f6f-472e-bf7b-b0e26b270698",
+      walletConnectors: [
+        EthereumWalletConnectors,
+        ZeroDevSmartWalletConnectorsWithConfig({
+          bundlerRpc:
+            "https://rpc.zerodev.app/api/v2/bundler/ba9facf9-1061-4e2f-925c-fe2e2e93d879?provider=PIMLICO",
+          paymasterRpc:
+            "https://rpc.zerodev.app/api/v2/paymaster/ba9facf9-1061-4e2f-925c-fe2e2e93d879?provider=PIMLICO",
+        }),
+      ],
     }}
   >
     <Main />
